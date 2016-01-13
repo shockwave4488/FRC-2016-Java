bc38c9f438aeadfecfa302b617bad0fec18dc0d4
lessthantrue <nickm@outofthisworld.net>
2016-01-13 12:18:00 -0800

    Initialized project
    Added:
    Coding guidelines files
    CSharpRoboticsLib DLL

---
+++ Robot2016/Robot2016/Robot2016.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WPILib;
+using WPILib.SmartDashboard;
+
+namespace Robot2016
+{
+    /// <summary>
+    /// The VM is configured to automatically run this class, and to call the
+    /// functions corresponding to each mode, as described in the IterativeRobot
+    /// documentation. 
+    /// </summary>
+    public class Robot2016 : IterativeRobot
+    {
+        /// <summary>
+        /// This function is run when the robot is first started up and should be
+        /// used for any initialization code.
+        /// </summary>
+        public override void RobotInit()
+        {
+        }
+
+        
+        /// <summary>
+        /// This function is run when the robot starts autonomous
+        /// </summary>
+        public override void AutonomousInit()
+        {
+        }
+
+        /// <summary>
+        /// This function is called periodically during autonomous
+        /// </summary>
+        public override void AutonomousPeriodic()
+        {
+        }
+
+        /// <summary>
+        /// This function is called periodically during operator control
+        /// </summary>
+        public override void TeleopPeriodic()
+        {
+
+        }
+
+        /// <summary>
+        /// This function is called periodically during test mode
+        /// </summary>
+        public override void TestPeriodic()
+        {
+
+        }
+    }
+}
