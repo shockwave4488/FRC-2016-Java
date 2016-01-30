1c49fc6baa0cab645fd240b712bd5fcc2e21c019
Isaac Pavon <isaac_pavon@yahoo.com>
2016-01-30 11:48:10 -0800

    Drive Stuff ported from c#

--- Robot2016/src/org/usfirst/frc/team4488/robot/systems/Drive.java
+++ Robot2016/src/org/usfirst/frc/team4488/robot/systems/Drive.java
@@ -5,21 +5,33 @@ import org.usfirst.frc.team4488.robot.RobotMap;
 import JavaRoboticsLib.Drive.Interfaces.*;
 import JavaRoboticsLib.WPIExtensions.*;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 
 public class Drive implements TankDrive{
 	
-	private SpeedControllerGroup m_left;
-	private SpeedControllerGroup m_right;
-	
-	private Encoder m_leftEncoder;
-	private Encoder m_rightEncoder;	
+	private CANTalon m_left;
+	private SpeedControllerGroup m_leftFollowers;
+	private CANTalon m_right;
+	private SpeedControllerGroup m_rightFollowers;
 	
 	public Drive() {
 		try {
-			m_left = new SpeedControllerGroup(CANTalon.class, new int[]{RobotMap.DriveMotorLeft1,RobotMap.DriveMotorLeft2,RobotMap.DriveMotorLeft3});
-			m_right = new SpeedControllerGroup(CANTalon.class, new int[]{RobotMap.DriveMotorRight1,RobotMap.DriveMotorRight2,RobotMap.DriveMotorRight3});
-			m_leftEncoder = new Encoder(RobotMap.DriveLeftEncoderA, RobotMap.DriveLeftEncoderB);
-			m_rightEncoder = new Encoder(RobotMap.DriveRightEncoderA,RobotMap.DriveRightEncoderB);		
+			m_left = new CANTalon(RobotMap.DriveMotorLeft1);
+			CANTalon leftSlave1 = new CANTalon(RobotMap.DriveMotorLeft2);
+            CANTalon leftSlave2 = new CANTalon(RobotMap.DriveMotorLeft3);
+            leftSlave1.changeControlMode(TalonControlMode.Follower);
+            leftSlave1.set(RobotMap.DriveMotorLeft1);
+            leftSlave2.changeControlMode(TalonControlMode.Follower);
+            leftSlave2.set(RobotMap.DriveMotorLeft1);
+            m_leftFollowers = new SpeedControllerGroup(new SpeedController[]{leftSlave1, leftSlave2});
+            m_right = new CANTalon(RobotMap.DriveMotorRight1);
+            CANTalon rightSlave1 = new CANTalon(RobotMap.DriveMotorRight2);
+            CANTalon rightSlave2 = new CANTalon(RobotMap.DriveMotorRight3);
+            rightSlave1.changeControlMode(TalonControlMode.Follower);
+            rightSlave1.set(RobotMap.DriveMotorRight1);
+            rightSlave2.changeControlMode(TalonControlMode.Follower);
+            rightSlave2.set(RobotMap.DriveMotorRight1);
+            m_rightFollowers = new SpeedControllerGroup(new SpeedController[]{rightSlave1, rightSlave2});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
