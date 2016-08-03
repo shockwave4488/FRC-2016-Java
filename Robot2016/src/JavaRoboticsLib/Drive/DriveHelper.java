1ad0a40e3c95e14b03fedb395473cc2bbd42ca1e
ShockwaveDriverStation <ShockwaveDriverStation@gmail.com>
2016-08-02 21:30:06 -0700

    Improved controller mapping
    Changed behavior at edge of deadzone to ramp up from 0 instead of step
    up to value

--- Robot2016/src/JavaRoboticsLib/Drive/DriveHelper.java
+++ Robot2016/src/JavaRoboticsLib/Drive/DriveHelper.java
@@ -150,7 +150,15 @@ public class DriveHelper {
 
   }
 
-  private static double handleDeadband(double val, double deadband) {
-    return (Math.abs(val) > Math.abs(deadband)) ? val : 0.0;
-  }
+	private static double handleDeadband(double val, double deadband) {
+	  if (Math.abs(val) > Math.abs(deadband)) {
+		  if (val > 0) {
+			  return (val-deadband)/(1-deadband);
+		  } else {
+			  return (val+deadband)/(1-deadband);
+		  }
+	  } else {
+		  return 0.0;
+	  }
+	}
 }
